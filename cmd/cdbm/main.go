cf9cab2c1977c15e07b614c96b94726f8d3f734a
xrzk <simon@conepsis.ch>
2026-01-14 22:36:35 +0100

    remove unused function

--- cmd/cdbm/main.go
+++ cmd/cdbm/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 
 	"github.com/xrzks/cdbm/internal/cli"
 	"github.com/xrzks/cdbm/internal/config"
@@ -27,16 +24,4 @@ func main() {
 	if err := app.Run(context.Background(), os.Args); err != nil {
 		log.Fatalf("Error: %v\n", err)
 	}
-}
-
-func BinaryExists(name string) (bool, error) {
-	_, err := exec.LookPath(name)
-	if err != nil {
-		if errors.Is(err, exec.ErrNotFound) {
-			return false, nil
-		}
-		return false, fmt.Errorf("unexpected lookup error: %w", err)
-	}
-
-	return true, nil
 }
