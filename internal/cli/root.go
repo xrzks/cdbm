a22768f28e7b676e3bf11eaaf89a478fd3d0dbba
xrzk <simon@conepsis.ch>
2026-03-22 00:04:00 +0100

    fix version to 0.2.0

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -23,7 +23,7 @@ func New(s *store.Store) *cli.Command {
 		Name:                  "cdbm",
 		Usage:                 "Manage directory bookmarks",
 		Aliases:               []string{"a"},
-		Version:               "0.3.0",
+		Version:               "0.2.0",
 		EnableShellCompletion: true,
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
